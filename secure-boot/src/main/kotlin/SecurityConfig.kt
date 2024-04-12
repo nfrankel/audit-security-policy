ajavageek/secure-boot

Move authentication logic from code to Apache APISIX

src/main/kotlin/SecurityConfig.kt
@@ -1,33 +1,42 @@
 package ch.frankel.blog.secureboot
 
-import org.springframework.context.support.beans
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.servlet.function.ServerRequest
+import org.springframework.web.servlet.function.ServerResponse
 
-
-internal fun security() = beans {
-    bean {
-        val props = ref<AppProperties>()
-        val webClient = WebClient.create(props.opaEndpoint)
-        OpaAuthenticationManager(ref(), webClient)
-    }
-    bean {
-        val http = ref<HttpSecurity>()
-        http {
-            authorizeRequests {
-                authorize("/finance/salary/**", authenticated)
-            }
-            addFilterBefore<UsernamePasswordAuthenticationFilter>(TokenAuthenticationFilter(ref()))
-            httpBasic { disable() }
-            csrf { disable() }
-            logout { disable() }
-            sessionManagement {
-                sessionCreationPolicy = SessionCreationPolicy.STATELESS
-            }
-        }
-        http.build()
-    }
+internal fun validateOpa(
+    opaWebClient: WebClient,
+    req: ServerRequest,
+    next: (ServerRequest) -> ServerResponse
+): ServerResponse {
+    val httpReq = req.servletRequest()
+    val account = httpReq.getHeader("X-Account")
+    val path = httpReq.servletPath.split('/').filter { it.isNotBlank() }
+    val decision = opaWebClient.post()
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(OpaInput(DataInput(account, path)))
+        .exchangeToMono { it.bodyToMono(DecisionOutput::class.java) }
+        .block() ?: DecisionOutput(ResultOutput(false))
+    return if (decision.result.allow) next(req)
+    else ServerResponse.status(HttpStatus.UNAUTHORIZED).build()
 }
+
+private data class OpaInput(
+    val input: DataInput
+)
+
+private data class DataInput(
+    val user: String,
+    val path: List<String>,
+)
+
+private data class DecisionOutput(
+    val result: ResultOutput
+)
+
+private data class ResultOutput(
+    val allow: Boolean,
+)

nfrankel/audit-security-policy

secure-boot/src/main/kotlin/SecurityConfig.kt
@@ -1,31 +1,41 @@
 package ch.frankel.blog.secureboot
 
-import org.springframework.context.support.beans
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.client.RestClient
+import org.springframework.web.servlet.function.ServerRequest
+import org.springframework.web.servlet.function.ServerResponse
 
-
-internal fun security() = beans {
-    bean {
-        val props = ref<AppProperties>()
-        val restClient = RestClient.create(props.opaEndpoint)
-        OpaAuthenticationManager(ref(), restClient)
-    }
-    bean {
-        val http = ref<HttpSecurity>()
-        http {
-            authorizeRequests {
-                authorize("/finance/salary/**", authenticated)
-            }
-            addFilterBefore<UsernamePasswordAuthenticationFilter>(TokenAuthenticationFilter(ref()))
-            httpBasic { disable() }
-            csrf { disable() }
-            logout { disable() }
-            sessionManagement { sessionCreationPolicy = SessionCreationPolicy.STATELESS }
-        }
-        http.build()
-    }
+internal fun validateOpa(
+    restClient: RestClient,
+    req: ServerRequest,
+    next: (ServerRequest) -> ServerResponse
+): ServerResponse {
+    val httpReq = req.servletRequest()
+    val account = httpReq.getHeader("X-Account")
+    val path = httpReq.servletPath.split('/').filter { it.isNotBlank() }
+    val decision = restClient.post()
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(OpaInput(DataInput(account, path)))
+        .exchange { _, resp -> resp.bodyTo(DecisionOutput::class.java) ?: DecisionOutput(ResultOutput(false)) }
+    return if (decision.result.allow) next(req)
+    else ServerResponse.status(HttpStatus.UNAUTHORIZED).build()
 }
+
+private data class OpaInput(
+    val input: DataInput
+)
+
+private data class DataInput(
+    val user: String,
+    val path: List<String>,
+)
+
+private data class DecisionOutput(
+    val result: ResultOutput
+)
+
+private data class ResultOutput(
+    val allow: Boolean,
+)